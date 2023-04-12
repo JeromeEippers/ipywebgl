46c800aa74baf40110bbd76ddac881a1c6d37f43
Jerome Eippers <jerome.eippers@ubisoft.com>
2023-04-12 15:44:30 +0200

    Bump to 0.2.2

--- ipywebgl/_version.py
+++ ipywebgl/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Jerome Eippers.
 # Distributed under the terms of the Modified BSD License.
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 version_info = tuple(int(i) for i in __version__.split('.'))
